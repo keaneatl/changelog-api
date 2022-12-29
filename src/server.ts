9f92a7c95ec7c6b246ab54c8a8de2829f2228aa3
Keane <kyntltntn@protonmail.com>
2022-12-30 02:59:47 +0800

    deploy

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express"
+import router from "./router"
+import morgan from "morgan"
+import { protect } from "./modules/auth"
+import { createNewUser, signIn } from "./handlers/user"
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    console.log("hello from express")
+    res.status(200)
+    res.json({message: "hello"})
+})
+
+app.use("/api", protect, router)
+
+app.post("/user", createNewUser)
+app.post("/signin", signIn)
+
+app.use((err, req, res, next) => {
+    if (err.type === "auth") {
+        res.status(401).json({message: "unauthorized"})
+    } else if (err.type === "input") {
+        res.status(400).json({message: "invalid input"})
+    } else {
+        res.status(500).json({message: "oops, that's on us"})
+    }
+})
+
+export default app
